13249ddee32e1fb660b1a7aa6c51af9d860da75e
lrennels <lrennels@berkeley.edu>
2021-07-02 12:07:52 -0700

    Cleanup duplicate files, add utils, etc

--- ciam-code/src/run_models.jl
+++ ciam-code/src/run_models.jl
@@ -1,26 +1,73 @@
 ## Run CIAM-Kopp and CIAM-BRICK 
-#using MimiCIAM
-include("MimiCIAM.jl")
-BRICKslr = ["BRICKsneasy-lsl_rcp85_p5.csv","BRICKsneasy-lsl_rcp85_p10.csv",
-"BRICKsneasy-lsl_rcp85_p50.csv","BRICKsneasy-lsl_rcp85_p90.csv","BRICKsneasy-lsl_rcp85_p95.csv"]
-KoppSlr = ["lsl_rcp85_p5.csv","lsl_rcp85_p50.csv","lsl_rcp85_p95.csv"]
-SSP=["IIASAGDP_SSP5_v9_130219"]
-SSP_simp = "5" # TWmod
+using MimiCIAM
 
-### Loop: Each run, modify init.txt with BRICK-SSP pairing, initialize model, write optimal results.
-runname="BRICKsneasy-IIASAGDP"  
-#textheader="run_name,lslr,subset,ssp\n"
-textheader="run_name,lslr,subset,ssp,ssp_simplified\n" # TWmod
+# pull in helper functions
+include("utils.jl")
+
+# create output directory
+outputdir = joinpath(@__DIR__, "..", "output", "SLR_runs")
+isdir(outputdir) || mkdir(outputdir)
+
+## BRICK RUNS
+
+BRICKslr = [
+    "BRICKsneasy-lsl_rcp85_p5.csv",
+    "BRICKsneasy-lsl_rcp85_p10.csv",
+    "BRICKsneasy-lsl_rcp85_p50.csv",
+    "BRICKsneasy-lsl_rcp85_p90.csv",
+    "BRICKsneasy-lsl_rcp85_p95.csv"
+]
+
+# For each run, create a new init.txt with BRICK-SSP pairing, initialize model, write 
+# optimal results.
 for b in BRICKslr
-#    textstr = "base,$(b),false,$(SSP[1])"
-    textstr = "base,$(b),false,$(SSP[1]),$(SSP_simp)" # TWmod
-    txtfile=open("../data/batch/init.txt","w") do io
-        write(io,textheader)
-        write(io,textstr)
-    end
-    m = MimiCIAM.get_model()
+
+    run_name = "BRICKsneasy-IIASAGDP_$(split(b, ".")[1])"
+
+    init_settings = Dict(
+        :init_filename   => string("$run_name", "_init.csv"),
+        :lslrfile        => b,
+        :subset          => false,
+        :ssp             => "IIASAGDP_SSP5_v9_130219",
+        :ssp_simplified  => 5
+    )
+
+    MimiCIAM.write_init_file(run_name, outputdir, init_settings)
+    m = MimiCIAM.get_model(initfile = joinpath(outputdir, init_settings[:init_filename]))
     run(m)
-    MimiCIAM.write_optimal_costs(m,runname=runname)
+
+    MimiCIAM.write_optimal_costs(m; outputdir = outputdir, runname = run_name)
 
 end
     
+## KOPP RUNS
+
+KoppSlr = [
+    "lsl_rcp0_p50.csv",
+    "lsl_rcp45_p50.csv",
+    "lsl_rcp85_p5.csv",
+    "lsl_rcp85_p50.csv",
+    "lsl_rcp85_p95.csv"
+]
+
+# For each run, create a new init.txt with BRICK-SSP pairing, initialize model, write 
+# optimal results.
+for k in KoppSlr
+
+    run_name = "BRICKsneasy-IIASAGDP_$(split(k, ".")[1])"
+
+    init_settings = Dict(
+        :init_filename   => string("$run_name", "_init.csv"),
+        :lslrfile        => k,
+        :subset          => false,
+        :ssp             => "IIASAGDP_SSP5_v9_130219",
+        :ssp_simplified  => 5
+    )
+
+    MimiCIAM.write_init_file(run_name, outputdir, init_settings)
+    m = MimiCIAM.get_model(initfile = joinpath(outputdir, init_settings[:init_filename]))
+    run(m)
+
+    MimiCIAM.write_optimal_costs(m; outputdir = outputdir, runname = run_name)
+
+end
