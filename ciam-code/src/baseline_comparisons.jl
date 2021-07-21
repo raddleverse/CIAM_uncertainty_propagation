b9d899664d9ad0b7b403f258236c4df4d6858427
Tony Wong <aewsma@rit.edu>
2021-07-21 06:53:24 -0400

    Update baseline_comparisons.jl
    Deleting high population check code

--- ciam-code/src/baseline_comparisons.jl
+++ ciam-code/src/baseline_comparisons.jl
@@ -6,7 +6,7 @@
 ##==============================================================================
 
 using Mimi
-using MimiCIAM#high_costs_population_check
+using MimiCIAM
 using CSV
 using RData
 using DataFrames
@@ -314,58 +314,6 @@ update_param!(m, :slrcost, :lslr, lslr[1,:,:])
 run(m)
 
 MimiCIAM.write_output_files(m, outputdir, run_name)
-
-# vvvvvvvvv DELETE BELOW HERE vvvvvvvvv
-
-# just checking to see if it's really just the population density data that makes the SSP5+RCP8.5 projections so much lower
-
-##==============================================================================
-##  baseline+updated SLR (RCP8.5)+updated GPD/POP via SSP5
-
-run_name = "ctrl+BRICKLSL85_popJones"
-
-init_settings = Dict(
-    :init_filename   => string("$run_name", "_init.csv"),
-    :lslrfile        => "lsl_rcp85_p50.csv",
-    :subset          => false,
-    :ssp             => "0",
-    :ssp_simplified  => "5"  #SSP = "IIASAGDP_SSP5_v9_130219"
-)
-
-model_settings = Dict(
-    :fixed          => true,
-    :t              => 15,
-    :noRetreat      => false,
-    :allowMaintain  => false,
-    :popinput       => 1, # 0 = default old stuff, 1 = Jones and O'Neill (2016), 2 = Merkens et al (2016)
-    :GAMSmatch      => false
-)
-
-MimiCIAM.write_init_file(run_name, outputdir, init_settings)
-
-m = MimiCIAM.get_model(
-    initfile        = joinpath(outputdir, init_settings[:init_filename]),
-    fixed           = model_settings[:fixed],
-    t               = model_settings[:t],
-    noRetreat       = model_settings[:noRetreat],
-    allowMaintain   = model_settings[:allowMaintain],
-    popinput        = model_settings[:popinput],
-    GAMSmatch       = model_settings[:GAMSmatch]
-)
-
-segIDs = get_segIDs(init_settings[:subset])
-rcp = 85
-lsl = brick_lsl(rcp,segIDs,brickfile,1,50,50,2010,2150,10,false) # end_year here only for picking median SLR ensemble member
-lslr=lsl[1]
-gmsl=lsl[2]
-ensInds=lsl[3] # Indices of original BRICK array
-update_param!(m, :slrcost, :lslr, lslr[1,:,:])
-
-run(m)
-
-MimiCIAM.write_output_files(m, outputdir, run_name)
-
-# ^^^^^^^ DELETE ABOVE HERE ^^^^^^^^
 
 ##==============================================================================
 ##  baseline+updated SLR (RCP2.6)+updated GPD/POP via SSP1
