4a7404f65e4af3581bb768dd13c6ba2c83b02e6b
Tony Wong <aewsma@rit.edu>
2022-06-09 07:30:11 -0400

    adding wider and narrower ciam parameter priors experiments

---
+++ ciam-code/src/montecarlo/defmcs_wider.jl
@@ -0,0 +1,29 @@
+using Distributions
+
+ciam_default_mcs = @defsim begin
+    #slrcost.movefactor  = TriangularDist(0.5,3,1)          # From Diaz (2016); incorporates communication with Mendelsohn and Anthoff and Tol (2014)
+    slrcost.movefactor  = Truncated(Normal(1,2),0.5,3)       # From Diaz (2016); incorporates communication with Mendelsohn and Anthoff and Tol (2014)
+    slrcost.dvbm        = Truncated(Normal(5.376,5.376),0.0,Inf) # Updated to 2010USD from FUND
+    slrcost.vslel       = Truncated(Normal(0.47,0.3),0.0,Inf) # From Viscusi and Aldy (2003)
+    slrcost.vslmult     = Truncated(Normal(200,200),0.0,Inf) # From FUND (originally Cline (1992))
+    slrcost.wvel        = Truncated(Normal(1.16, 0.92),0.0,Inf) # From Brander et al (2006)
+    slrcost.wvpdl       = Truncated(Normal(0.47,0.24),0.0,1.0) # From Brander et al (2006)
+end
+
+# hack to use the same interface but only sample a fixed central value
+ciam_fixed_mcs = @defsim begin
+    slrcost.movefactor  = TriangularDist(1,1,1)          # From Diaz (2016); incorporates communication with Mendelsohn and Anthoff and Tol (2014)
+    slrcost.dvbm        = TriangularDist(5.376,5.376,5.376)    # Updated to 2010USD from FUND
+    slrcost.vslel       = TriangularDist(0.47,0.47,0.47)     # From Viscusi and Aldy (2003)
+    slrcost.vslmult     = TriangularDist(200,200,200)      # From FUND (originally Cline (1992))
+    slrcost.wvel        = TriangularDist(1.16,1.16,1.16)      # From Brander et al (2006)
+    slrcost.wvpdl       = TriangularDist(0.47,0.47,0.47)     # From Brander et al (2006)
+end
+
+function getmcs(vary_ciam)
+    if vary_ciam
+        return deepcopy(ciam_default_mcs)
+    else
+        return deepcopy(ciam_fixed_mcs)
+    end
+end
---
+++ ciam-code/src/montecarlo_driver_wider.jl
@@ -0,0 +1,144 @@
+##==============================================================================
+## montecarlo_driver.jl
+##
+## Tony Wong (aewsma@rit.edu)
+##==============================================================================
+
+using Mimi
+using MimiCIAM
+using RData
+using Query
+using StatsBase
+using NetCDF
+using DataFrames
+
+include("montecarlo/ciamMonteCarlo.jl")
+include("montecarlo/defmcs_wider.jl")
+include("montecarlo/run_ciam_mcs.jl")
+include("brickLSL.jl")
+include("processResults.jl")
+
+# The file structure created by this process will look as follows:
+#
+# - top directory: output/MonteCarlo holding subdirectories and the init file MCdriver_init.csv
+#
+# - for each call to runTrials, a (unique) subdir created with the code:
+#   joinpath(outputdir, runname, "CIAM $(Dates.format(now(), "yyyy-mm-dd HH-MM-SS")) MC$(trial_params[:n])")
+#
+# - for each of these subdirs, you will see a RawResults folder with any results directly
+#   from the monte carlo runs in run_ciam_mcs.jl and a PostProcessing folder written
+#   with the code at the bottom of ciamMonteCarlo.jl
+#
+
+# First, set some things up:
+
+#brickfile = joinpath(@__DIR__, "..", "data", "lslr", "BRICK_projections.RData")
+brickfile = "https://zenodo.org/record/6626335/files/sneasybrick_projections_csv.zip"
+#brickfile = joinpath(@__DIR__, "..", "data", "lslr", "sneasybrick_projections_csv.zip")
+outputdir = joinpath(@__DIR__, "..", "output", "MonteCarlo")
+isdir(outputdir) || mkpath(outputdir)
+
+ssp_files = Dict(1 => "IIASAGDP_SSP1_v9_130219",
+                 2 => "IIASAGDP_SSP2_v9_130219",
+                 3 => "IIASAGDP_SSP3_v9_130219",
+                 4 => "IIASAGDP_SSP4_v9_130219",
+                 5 => "IIASAGDP_SSP5_v9_130219")
+popinput = 0                          # population density input data (only 0 is supported currently)
+ssp_rcp_scenarios = [(1,26), (2,45), (4,60), (5,85)]  # what combinations of SSP (first) and RCP (second)?
+nensemble = 1000                      # how many ensemble members for the Monte Carlo?
+#surgeoptions = [0,1,2]                # which surge data sets to use (0 = original CIAM/DINAS-COAST; 1 = GTSR-corrected D-C; 2 = GTSR nearest data points)
+#TESTING:
+#ssp_rcp_scenarios = [(5,85)]  # what combinations of SSP (first) and RCP (second)?
+#ssp_rcp_scenarios = [(1,26), (2,45), (4,60), (5,85)]  # what combinations of SSP (first) and RCP (second)?
+#nensemble = 10                      # how many ensemble members for the Monte Carlo?
+surgeoptions = [0]                # which surge data sets to use (0 = original CIAM/DINAS-COAST; 1 = GTSR-corrected D-C; 2 = GTSR nearest data points)
+
+# Now, we actually do the simulations
+for surgeoption in surgeoptions
+    println("Surge option:",surgeoption,"...")
+
+    for (ssp, rcp) in ssp_rcp_scenarios
+
+        println("Running SSP",ssp, "-RCP",rcp,"...")
+
+        # write the init file
+        init_settings = Dict(
+            :init_filename   => "MCdriver_init.csv",
+            :subset          => false,
+            :ssp             => ssp_files[ssp],
+            :ssp_simplified  => ssp,
+            :rcp             => rcp,
+            :b => "lsl_rcp85_p50.csv" # won't matter, overwritten when doing the SLR sampling anyway
+        )
+        init_file = joinpath(outputdir,init_settings[:init_filename])
+        textheader="run_name,lslr,subset,ssp,ssp_simplified\n"
+        textstr = "base,$(init_settings[:b]),$(init_settings[:subset]),$(init_settings[:ssp]),$(init_settings[:ssp_simplified])"
+        txtfile=open(init_file,"w") do io
+            write(io,textheader)
+            write(io,textstr)
+        end
+
+        # Define input trial parameters: BRICK model, number of trials (n), min and max BRICK percentile,
+        # start and end year, and timestep. Resetting each time through the loop for consistency.
+        trial_params = Dict(
+            :brickfile  => brickfile,
+            :n      => nensemble,
+            :high   => 100,
+            :low    => 0,
+            :ystart => 2010,
+            :yend   => 2150,
+            :tstep  => 10,
+        )
+
+        # Define other eneded parameters and settings for the model
+        adaptRegime1 = Dict(
+            :fixed          => true,
+            :t              => 15,
+            :noRetreat      => false,
+            :allowMaintain  => false,
+            :popval         => popinput,
+            :GAMSmatch      => false,
+            :surgeoption    => surgeoption,
+            :subset         => false
+        )
+
+        # vary SLR and CIAM parameters
+        println("... vary SLR and vary CIAM ...")
+        trial_params[:low] = 0
+        trial_params[:high] = 100
+        runname = string("SSP",init_settings[:ssp_simplified],"_BRICK",init_settings[:rcp],"_surge",surgeoption,"_varySLR_varyCIAM_wider")
+        runTrials(init_settings[:rcp], init_settings[:ssp_simplified], trial_params, adaptRegime1, outputdir, init_file, vary_slr=true, vary_ciam=true, runname=runname)
+
+        # only 5th percentile of SLR, with CIAM defaults
+        println("... 5th percentile ...")
+        prctile = 5
+        trial_params[:low] = prctile
+        trial_params[:high] = prctile
+        trial_params[:n] = 1
+        runname = string("SSP",init_settings[:ssp_simplified],"_BRICK",init_settings[:rcp],"_surge",surgeoption,"_p",prctile,"_wider")
+        runTrials(init_settings[:rcp], init_settings[:ssp_simplified], trial_params, adaptRegime1, outputdir, init_file, vary_slr=false, vary_ciam=false, runname=runname)
+
+        # only 50th percentile of SLR, with CIAM defaults
+        println("... 50th percentile ...")
+        prctile = 50
+        trial_params[:low] = prctile
+        trial_params[:high] = prctile
+        trial_params[:n] = 1
+        runname = string("SSP",init_settings[:ssp_simplified],"_BRICK",init_settings[:rcp],"_surge",surgeoption,"_p",prctile,"_wider")
+        runTrials(init_settings[:rcp], init_settings[:ssp_simplified], trial_params, adaptRegime1, outputdir, init_file, vary_slr=false, vary_ciam=false, runname=runname)
+
+        # only 95th percentile of SLR, with CIAM defaults
+        println("... 95th percentile ...")
+        prctile = 95
+        trial_params[:low] = prctile
+        trial_params[:high] = prctile
+        trial_params[:n] = 1
+        runname = string("SSP",init_settings[:ssp_simplified],"_BRICK",init_settings[:rcp],"_surge",surgeoption,"_p",prctile,"_wider")
+        runTrials(init_settings[:rcp], init_settings[:ssp_simplified], trial_params, adaptRegime1, outputdir, init_file, vary_slr=false, vary_ciam=false, runname=runname)
+
+    end
+end
+
+##==============================================================================
+## End
+##==============================================================================
